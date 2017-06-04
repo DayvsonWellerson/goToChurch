c24079031f439109314e22ba58b60389a9fea496
Marcelo Barros <marcelobjr@gmail.com>
2017-06-04 19:24:49 -0300

    api login test

--- jsp/api/login.java
+++ jsp/api/login.java
@@ -34,7 +34,8 @@ public class login extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		response.sendError(401, "NTO AUTHORIZATION");
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
